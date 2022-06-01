9632e20e7a162437fdabb3f7ec6d571fcc6449fe
Abián Izquierdo <abizmo@gmail.com>
2022-06-01 15:57:36 +0100

    fix: add a new size

--- src/Theme/theme.ts
+++ src/Theme/theme.ts
@@ -5,6 +5,8 @@ const theme = {
     500: 'hsl(180, 29%,50%)',
     600: 'hsl(180, 8%, 52%)',
     800: 'hsl(180, 14%, 20%)',
+    black: 'hsl(180, 0%, 0%)',
+    white: 'hsl(180, 100%, 100%)',
   },
   fontSizes: {
     200: '.6875rem',
@@ -12,19 +14,20 @@ const theme = {
     500: '.9375rem',
     600: '1rem',
     700: '1.125rem',
-    900: '1.375rem'
+    900: '1.375rem',
   },
   fontFamily: "'League Spartan', sans-serif",
   sizes: {
     4: '.25rem',
     8: '.5rem',
+    12: '.75rem',
     16: '1rem',
     20: '1.25rem',
     24: '1.5rem',
     32: '2rem',
     48: '3rem',
     88: '5.5rem',
-  }
+  },
 };
 
 export default theme;
